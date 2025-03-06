17cd7ad1b2539bd74f31c687e8d47a45e270f0fc
Roxanaa1 <roxanaenache2003@gmail.com>
2025-03-06 16:32:30 +0200

    Reservation Scheduler

--- src/main/java/com/example/Library/repository/ReservationRepository.java
+++ src/main/java/com/example/Library/repository/ReservationRepository.java
@@ -1,9 +1,16 @@
 package com.example.Library.repository;
 
 import com.example.Library.entities.Reservation;
+import com.example.Library.entities.StatusReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByStatusReservationAndStartDate(StatusReservation status, LocalDate startDate);
+
+    List<Reservation> findByStatusReservationAndEndDate(StatusReservation status, LocalDate endDate);
 }
